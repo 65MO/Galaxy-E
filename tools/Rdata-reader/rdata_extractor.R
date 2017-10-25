3e04d323ed21fb8bee3a8460b6055a79995a1feb
Alanamosse <alan.amosse@gmail.com>
2017-10-25 11:57:27 +0200

    Add flexible file ext and function

--- tools/Rdata-reader/rdata_extractor.R
+++ tools/Rdata-reader/rdata_extractor.R
@@ -16,23 +16,30 @@ attributes<-strsplit(attributes_selected, ",") #List of elements
 write.table(sum,file = "summary.tsv")
 len<-length(attributes[[1]])
 
+#file type definition
+file_ext<-function(ext){
+	file<-paste(attributes[[1]][i],ext,sep="") #Filename definition
+	return(file)
+}
+
 for (i in 1:len){
 	attribute<-attributes[[1]][i] #Get the attribute i 
 	if(! any(names(rdata)==attribute)){
 		error<-paste(attribute, " doesn't exist in the RData. Check the inputs files")
 		write(error, stderr())
 	}
-	file<-paste(attributes[[1]][i],".tsv",sep="") #Filename definition
 
 	attribute_val<-eval(parse(text=paste("rdata$",attribute,sep=""))) #Extract the value(s)
 
 	if(is.null(attribute_val)){ #Galaxy can't produce output if NULL
-		write.table("Return NULL value",file = file)
+		file<-file_ext(".txt")
+		write("Return NULL value",file = file)
 		next #Exit loop
 	}
 
 	if (typeof(attribute_val)=="list"){ #Need to be corrected, fail in galaxy but not in R
 		if(length(attribute_val)=="0"){
+			file<-file_ext(".txt")
 			sink(file=file)
 			print("Empty list :") #If the list is empty without element, file is empty and an error occur in galaxy
 			print(attribute_val)
@@ -40,10 +47,16 @@ for (i in 1:len){
 			next
 		}else{
 			attribute_val<-as.data.frame(do.call(rbind, attribute_val))
+			file<-file_ext(".tsv")
+			write.table(attribute_val,file = file)
+			next
 		}
 	}else if (typeof(attribute_val)=="language"){ #OK
 		attribute_val<-toString(attribute_val,width = NULL)
+		file<-file_ext(".txt")
+		write(attribute_val,file = file)
+		next
 	}
-	write.table(attribute_val,file = file)
-
+	file<-file_ext(".tsv")
+	write.table(attribute_val,file = file)	
 }
