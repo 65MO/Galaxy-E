0899c9ab012b74cfc74760efc73430d07e4c32eb
Yvan Le Bras <yvan.le_bras@mnhn.fr>
2018-02-01 20:41:41 +0100

    Add GIS shiny GIE

---
+++ GIE/shiny-GIE-GIS/global.r
@@ -0,0 +1,13 @@
+# global.R
+# Code here is in scope to both ui.R and server.R
+
+#Load required library
+library(leaflet)
+
+# Get dataset from CSV
+inputstn <- read.csv("/srv/shiny-server/data/inputdata.txt", header=TRUE)
+
+# Create a vector of country subdivions (i.e. regions in France)
+#inputstn <- subset(inputstn, Country == "FR" )
+regions <- unique(as.vector(inputstn$DEPNAME))
+
