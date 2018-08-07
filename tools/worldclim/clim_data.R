b178299f2fddfcf634e50efb46ec8b762714b0a5
Alanamosse <alan.amosse@gmail.com>
2018-08-07 17:24:17 +0200

    Add custom plots regions and titles

--- tools/worldclim/clim_data.R
+++ tools/worldclim/clim_data.R
@@ -4,22 +4,50 @@ args <- commandArgs(trailingOnly = TRUE)
 
 #Rscript clim_data.R 'worldclim' 'var' 'resolution' 'OutputFormat' #'FRA' #'prec1'
 if (length(args)==0 | args[1]=="-h" | args[1]=="--help"){
-  print ("Rscript clim_data.R \'worldclim\' \'var\' resolution \'OutputFormat\' #\'FRA' #\'prec1\'")
+  print ("general script execution : Rscript clim_data.R \'worldclim\' \'var\' resolution \'OutputFormat\' #\'variable-to-plot\' #\'region_code\'")
+  print ("eg : Rscript clim_data.R \'worldclim\' \'prec\' 10 \'raster\' #\'prec1' #\'FRA\'")
   q('no')
 } 
 
-library('raster')#,quietly = TRUE, warn.conflicts = FALSE) 
+
+#Climatic variables dictionaries
+months<-c("January","February","March","April","May","June","July","August","September","October","November","December")
+bioclimatic_vars<-c("Annual Mean Temperature", "Mean Diurnal Range (Mean of monthly (max temp - min temp))", "Isothermality (BIO2/BIO7) (x 100)","Temperature Seasonality (standard deviation x100)","Max Temperature of Warmest Month","Min Temperature of Coldest Month","Temperature Annual Range (BIO5-BIO6)","Mean Temperature of Wettest Quarter","Mean Temperature of Driest Quarter","Mean Temperature of Warmest Quarter","Mean Temperature of Coldest Quarter","Annual Precipitation","Precipitation of Wettest Month","Precipitation of Driest Month","Precipitation Seasonality (Coefficient of Variation)","Precipitation of Wettest Quarter","Precipitation of Driest Quarter","Precipitation of Warmest Quarter","Precipitation of Coldest Quarter")
+
+#Function to create custom plot title
+get_plot_title<-function(usr_var,usr_var_to_plot){
+  match<-str_extract(usr_var_to_plot,"[0-9]+")
+  if(usr_var %in% c("prec","tmin","tmax")){
+      printable_var<-(months[as.integer(match)])
+      if(usr_var=="prec"){
+        printable_var<-paste(printable_var," precipitations (mm)",sep="")
+      }else if(usr_var=="tmin"){
+        printable_var<-paste(printable_var," minimum temperature (°C *10)",sep="")
+      }else{
+        printable_var<-paste(printable_var," maximum temperature (°C *10)",sep="")
+      }
+  }else if(usr_var=="bio"){
+      printable_var<-(bioclimatic_vars[as.integer(match)])
+      printable_var<-paste("Bioclimatic variable - ",printable_var,sep="")
+  }
+  title<-paste("Worldclim data - ",printable_var,".",sep="")
+  return(title)
+}
+
+
+#Call libraries
+library('raster') 
 library(sp,quietly = TRUE, warn.conflicts = FALSE)
-##library('Cairo')
 library(ncdf4,quietly = TRUE, warn.conflicts = FALSE)
-##library(rgdal,quietly = TRUE, warn.conflicts = FALSE)
+##library(rgdal,quietly = TRUE, warn.conflicts = FALSE) #To save as geotif
+library(stringr)
 
 
+#Get args
 usr_data=args[1]
 usr_var=args[2]
 usr_res=as.numeric(args[3])
 usr_of=args[4]
-
 
 
 # Retrieve 'var' data from WorldClim
@@ -35,67 +63,73 @@ if (length(global.var)==0){
 }
 
 
+
+
+
+
 #################
 ##Visualisation##
 #################
 
+#Get args
 if(length(args[5])>=0 && length(args[6])>=0){
   usr_var_to_plot=args[5]
   usr_plot_region=args[6]
+}else{q('no')}
 
-  list_country_mask<-c("FRA","DEU","GBR","ESP","ITA")
+list_region_mask<-c("FRA","DEU","GBR","ESP","ITA")
 
-  if(usr_plot_region %in% list_country_mask){
-  #Country mask
-    country <- getData("GADM",country=usr_plot_region,level=0)
-    country_mask <- mask(global.var, country)
-    country_var_to_plot_expression<-paste("country_mask$",usr_var_to_plot,sep="")
-  }else{ #All map and resize manualy
-    country_var_to_plot_expression<-paste("global.var$",usr_var_to_plot,sep="")
-  }
+if(usr_plot_region %in% list_region_mask){
+#Country mask
+  region <- getData("GADM",country=usr_plot_region,level=0)
+  region_mask <- mask(global.var, region)
+  region_var_to_plot_expression<-paste("region_mask$",usr_var_to_plot,sep="")
+}else{ #All map and resize manualy
+  region_var_to_plot_expression<-paste("global.var$",usr_var_to_plot,sep="")
+}
 
 
-  country_var_to_plot<-eval(parse(text=country_var_to_plot_expression))
+region_var_to_plot<-eval(parse(text=region_var_to_plot_expression))
 
-####### PLOT LIM PER usr_plot_region
+#PLotmap
+jpeg(file="worldclim_plot_usr_region.jpeg",bg="white")
 
-  #PLotmap
-  jpeg(file="worldclim_plot_usr_country.jpeg",bg="white")
+title<-get_plot_title(usr_var,usr_var_to_plot)
+print(title)
 
-  if(usr_plot_region=="FRA"){
-    #FRA
-    plot(country_var_to_plot, xlim = c(-7, 12), ylim = c(40, 52), axes=TRUE,xlab="Longitude",ylab="Latitude",main="Worldclim data - France")
-  }else if(usr_plot_region=="GBR"){
-    #GBR
-    plot(country_var_to_plot, xlim = c(-10, 5), ylim = c(46, 63), axes=TRUE,xlab="Longitude",ylab="Latitude",main="Worldclim data - UK")
-  }else if(usr_plot_region=="NA"){
-    #North America : 
-    plot(country_ar_to_plot,xlim=c(-180,-50),ylim=c(10,75),xlab="Longitude",ylab="Latitude",main="Worldclim data - North America")
-  }else if(usr_plot_region=="EU"){
-    #Europe
-    plot(country_var_to_plot,xlim=c(-28,48),ylim=c(34,72),xlab="Longitude",ylab="Latitude",main="Worldclim data - Europe")
-  }else if(usr_plot_region=="DEU"){
-    #DEU
-    plot(country_var_to_plot, xlim = c(5, 15), ylim = c(45, 57),axes=TRUE,xlab="Longitude",ylab="Latitude",main="Worldclim data - Germany")
-  }else if(usr_plot_region=="ESP"){
-    #ESP
-    plot(country_var_to_plot, xlim = c(-10, 6), ylim = c(35, 45), axes=TRUE,xlab="Longitude",ylab="Latitude", main="Worldclim data - Spain")
-  }else if(usr_plot_region=="ITA"){
-    #ITA
-    plot(country_var_to_plot, xlim = c(4, 20), ylim = c(35, 48), axes=TRUE,xlab="Longitude",ylab="Latitude", main="Worldclim data - Italy")
-  }else if(usr_plot_region=="WM"){
-    #Worldmap
-    plot(country_var_to_plot,xlab="Longitude",ylab="Latitude",main="Worldclim data")
-  }else if(usr_plot_region=="AUS"){
-    #AUS
-    plot(country_var_to_plot,xlim=c(110,155),ylim=c(-45,-10),xlab="Longitude",ylab="Latitude",axes=TRUE,main="Worldclim data - Australia")
-  }else{
-    write("Error with country code.", stderr())  
-    q('no')
-  }
+if(usr_plot_region=="FRA"){
+  #FRA
+  plot(region_var_to_plot, xlim = c(-7, 12), ylim = c(40, 52), axes=TRUE,xlab="Longitude",ylab="Latitude",main=title)
+}else if(usr_plot_region=="GBR"){
+  #GBR
+  plot(region_var_to_plot, xlim = c(-10, 5), ylim = c(46, 63), axes=TRUE,xlab="Longitude",ylab="Latitude",main=title)
+}else if(usr_plot_region=="NA"){
+  #North America : 
+  plot(region_ar_to_plot,xlim=c(-180,-50),ylim=c(10,75),xlab="Longitude",ylab="Latitude",main=title)
+}else if(usr_plot_region=="EU"){
+  #Europe
+  plot(region_var_to_plot,xlim=c(-28,48),ylim=c(34,72),xlab="Longitude",ylab="Latitude",main=title)
+}else if(usr_plot_region=="DEU"){
+  #DEU
+  plot(region_var_to_plot, xlim = c(5, 15), ylim = c(45, 57),axes=TRUE,xlab="Longitude",ylab="Latitude",main=title)
+}else if(usr_plot_region=="ESP"){
+  #ESP
+  plot(region_var_to_plot, xlim = c(-10, 6), ylim = c(35, 45), axes=TRUE,xlab="Longitude",ylab="Latitude", main=title)
+}else if(usr_plot_region=="ITA"){
+  #ITA
+  plot(region_var_to_plot, xlim = c(4, 20), ylim = c(35, 48), axes=TRUE,xlab="Longitude",ylab="Latitude", main=title)
+}else if(usr_plot_region=="WM"){
+  #Worldmap
+  plot(region_var_to_plot,xlab="Longitude",ylab="Latitude",main=title)
+}else if(usr_plot_region=="AUS"){
+  #AUS
+  plot(region_var_to_plot,xlim=c(110,155),ylim=c(-45,-10),xlab="Longitude",ylab="Latitude",axes=TRUE,main=title)
+}else{
+  write("Error with country code.", stderr())  
+  q('no')
+}
 
-  dev.off
-} 
-
+dev.off
+ 
 #Exit
 q('no')
