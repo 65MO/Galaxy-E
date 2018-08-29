ed56a6c14b4daaef57336a04c94afc772bcda273
ValentinChCloud <chavaldranis@gmail.com>
2018-08-29 09:08:06 +0000

    Add import galaxy files

--- GIE/GIE_Biodiv/GIE_Shiny_vegan/vegan_gui-master/server.R
+++ GIE/GIE_Biodiv/GIE_Shiny_vegan/vegan_gui-master/server.R
@@ -8,7 +8,8 @@ for (f in list.files('./modules')) {
 }
 
 function(input, output) {
-
+File_1<-NULL
+File_2<-NULL
 
   getData <- reactive({
 
@@ -17,20 +18,20 @@ function(input, output) {
     if (is.null(input$file1))
       return(NULL)
 
-    read.csv(inFile_1$datapath)
+    File_1<<-read.csv(inFile_1$datapath)
 
   })
 
 
 galaxyOccs<- callModule(galaxyOccs_MOD, 'c1_galaxyOccs', rvs)
   observeEvent(input$galaxyfile1,{
-   inFile_1 <- galaxyOccs()
-   output$rawdata<- renderDataTable(inFile_1)
+   File_1 <<- galaxyOccs()
+   output$rawdata<- renderDataTable(File_1)
 })
 
    observeEvent(input$galaxyfile2,{
-   inFile_2 <- galaxyOccs()
-   output$rawdata_2<- renderDataTable(inFile_2)
+   File_2 <<- galaxyOccs()
+    output$rawdata_2<- renderDataTable(File_2)
 })
    getData_2 <- reactive({
 
@@ -39,7 +40,7 @@ galaxyOccs<- callModule(galaxyOccs_MOD, 'c1_galaxyOccs', rvs)
     if (is.null(input$file2))
       return(NULL)
 
-    read.csv(inFile_2$datapath)
+    File_2 <<-read.csv(inFile_2$datapath)
 
   })
 
@@ -50,20 +51,20 @@ galaxyOccs<- callModule(galaxyOccs_MOD, 'c1_galaxyOccs', rvs)
 
   output$rarecurve <- renderPlot({
     withProgress(message = 'Computing rarefaction curves', value = 0.45, {
-      rarecurve(getData(), main = "Rarefaction Curves")
+      rarecurve(File_1, main = "Rarefaction Curves")
     })
 
   })
 
   output$diversity <- renderPlot({
     withProgress(message = 'Computing diversity', value = 0.40, {
-      barplot(diversity(inFile_1, index = input$variable), main = "Diverstity across sites", names.arg = seq(1, dim(inFile_1)[1]))
+      barplot(diversity(File_1, index = input$variable), main = "Diverstity across sites", names.arg = seq(1, dim(File_1)[1]))
     })
   })
 
   output$ordination <- renderPlot({
     withProgress(message = 'Computing ordination', value = 0.45, {
-      ord <- metaMDS(getData())
+      ord <- metaMDS(File_1)
       plot(ord, type = "n")
       points(ord, display = "sites", cex = 0.8, pch = 21, col = "red", bg = "yellow")
       text(ord, display = "spec", cex = 0.7, col = "blue")
@@ -72,14 +73,14 @@ galaxyOccs<- callModule(galaxyOccs_MOD, 'c1_galaxyOccs', rvs)
 
   output$ordination_stress <- renderPlot({
     withProgress(message = 'Computing stress', value = 0.55, {
-      ord <- metaMDS(getData())
+      ord <- metaMDS(File_1)
       stressplot(ord)
     })
   })
 
   output$ordination_env_fit <- renderPlot({
     withProgress(message = 'Computing CCA', value = 0.55, {
-      ord <- cca(getData() ~ ., getData_2())
+      ord <- cca(File_1 ~ ., File_2)
       plot(ord)
     })
   })
