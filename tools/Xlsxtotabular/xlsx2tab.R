4d94a82ec5969fdd1194b0463765205a455fef71
Yvan Le Bras <yvan.le-bras@mnhn.fr>
2018-12-03 15:55:39 +0100

    WIP

---
+++ tools/Xlsxtotabular/xlsx2tab.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+#Read Xlsx sheets - return tabular
+library(readxl)
+
+args = commandArgs(trailingOnly=TRUE)
+
+sheet<-read_excel(args[1],sheet=args[2])
+write.table(sheet,"out.tabular",sep="\t",row.names=FALSE)
+
+}
+
