8e35e484dc556ce18723f7978d900a9c7f5dc917
Yvan Le Bras <yvan.le-bras@mnhn.fr>
2018-12-06 18:55:51 +0100

    Delete xlsx2tab.R

--- tools/Xlsxtotabular/xlsx2tab.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-#Read Xlsx sheets - return tabular
-library(readxl)
-
-args = commandArgs(trailingOnly=TRUE)
-
-sheet<-read_excel(args[1],sheet=args[2])
-write.table(sheet,"out.tabular",sep="\t",row.names=FALSE)
